d268cae799bc7a5d19b48bf7e6de14b91a316c5d
Csedem0 <emmasobula@gmail.com>
2024-10-22 01:39:50 +0100

    sec

--- prediction/settings.py
+++ prediction/settings.py
@@ -21,16 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = '_<xbgbgV|\'@!i|=fNnKe[Dv$;GUjcA)mMd=ZUO;PE{LlO\\;<P5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = '_<xbgbgV|\'@!i|=fNnKe[Dv$;GUjcA)mMd=ZUO;PE{LlO\\;<P5'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 DEBUG = True  # Set to False in production
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+# ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
 
 # Application definition
 
