c5f279612bd51cd9df7331f45baadd9ac60c632a
Csedem0 <emmasobula@gmail.com>
2024-10-22 21:18:36 +0100

    Debug True

--- prediction/settings.py
+++ prediction/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = False  # Set to False in production
+DEBUG = True  # Set to False in production
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 # ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
